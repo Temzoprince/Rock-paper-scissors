3585f3dcc25eaa8d53bcff3778fb49353bd15062
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-03-18 19:17:18 +0000

    Plan one round of RPS

---
+++ javascript.js
@@ -0,0 +1,22 @@
+/* 
+Rock paper scissors 
+
+Classic RPS vs the computer for 5 rounds
+The winner of each round will be displayed and then the overall winner after
+the 5th round
+The overall winner is the one who wins 3 out of the 5 games
+
+How to win:
+Player and component will choose either rock paper or scissors
+User wins by choosing a superior option
+Rock beats Scissors
+Scissors beats Paper
+Paper beats Rock
+*/
+
+// Get the computers choice - a random choice of rock, paper or scissors
+// Play a round of rock paper scissors
+    // Ask the user to enter their selection
+    // Store that choice in a variable
+    // Compare the user's choice to the computer's choice
+    // Display the winner of the round in the console
