17bc164b3ee112d5d30648344a659ef738b578d6
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-03-19 07:59:56 +0000

    Add functionality for 5 rounds
    The game will run through 5 rounds, display the score and overall
    winner

--- javascript.js
+++ javascript.js
@@ -17,7 +17,7 @@ Paper beats Rock
 // Get the computers choice - a random choice of rock, paper or scissors
 let getComputerChoice = function() {
     let choice = Math.floor(Math.random() * 3);
-    console.log(choice);
+    // console.log(choice);
 
     if (choice === 0) {
         return "rock";
@@ -59,38 +59,80 @@ function playRound(computerSelection, playerSelection) {
     // When Computer chooses Rock
         // If you choose rock, it's a draw
     if ((computerSelection === "rock") && (playerSelection === "rock")) {
-        return "Draw! Both Players chose Rock";
+        console.log("Draw! Both Players chose Rock");
+        return "draw";
         // If you choose paper, you win
     } else if ((computerSelection === "rock") && (playerSelection === "paper")) {
-        return "You Win! Paper beats Rock";
+        console.log("You Win! Paper beats Rock");
+        return "win";
         // If you choose scissors, you lose
     } else if ((computerSelection === "rock") && (playerSelection === "scissors")) {
-        return "You Lose! Rock beats Scissors";
+        console.log("You Lose! Rock beats Scissors");
+        return "lose";
     }
 
     // When Computer Chooses Paper
         // If you choose rock, you lose
     if ((computerSelection === "paper") && (playerSelection === "rock")) {
-        return "You Lose! Paper beats Rock";
+        console.log("You Lose! Paper beats Rock");
+        return "lose";
         // If you choose paper, it's a draw
     } else if ((computerSelection === "paper") && (playerSelection === "paper")) {
-        return "Draw! Both Players chose Paper";
+        console.log("Draw! Both Players chose Paper");
+        return "draw";
         // If you choose scissors, you win
     } else if ((computerSelection === "paper") && (playerSelection === "scissors")) {
-        return "You Win! Scissors beats Paper";
+        console.log("You Win! Scissors beats Paper");
+        return "win";
     }
 
     // When Computer Chooses Scissors
         // If you choose rock, you win
     if ((computerSelection === "scissors") && (playerSelection === "rock")) {
-        return "You Win! Rock beats Scissors";
+        console.log("You Win! Rock beats Scissors");
+        return "win";
         // If you choose paper, you lose
     } else if ((computerSelection === "scissors") && (playerSelection === "paper")) {
-        return "You Lose! Scissors beats Paper";
+        console.log("You Lose! Scissors beats Paper");
+        return "lose";
         // If you choose scissors, it's a draw
     } else if ((computerSelection === "scissors") && (playerSelection === "scissors")) {
-        return "Draw! Both Players chose Scissors";
+        console.log("Draw! Both Players chose Scissors");
+        return "draw";
     }
 }
     // Display the winner of the round in the console
-    console.log(playRound(getComputerChoice(), getPlayerChoice()))
+    // console.log(playRound(getComputerChoice(), getPlayerChoice()))
+
+    function playGame() {
+        alert("Welcome to Rock Paper Scissors");
+        // Set two variables to record your score and the computer's score
+        let playerScore = 0;
+        let computerScore = 0;
+        // Run the game 5 times
+        for (let i = 0; i < 5; i++) {
+            // During each round, record who wins
+            let result = playRound(getComputerChoice(), getPlayerChoice())
+            if (result === "win") {
+                playerScore++
+            } else if (result === "lose") {
+                computerScore++
+            } else {
+                playerScore++
+                computerScore++
+            }
+        }
+        
+        // Display the overall winner and the score
+        console.log("\n" + "Player score is " + playerScore)
+        console.log("Computer score is " + computerScore)
+
+        if (playerScore > computerScore) {
+            console.log("\nYou Win! Congratulations!")
+        } else if (playerScore < computerScore) {
+            console.log("\nYou lose. Try again next time.")
+        } else {
+            console.log("\nIts a draw! Give it another shot.")
+        }
+    }
+    playGame();
