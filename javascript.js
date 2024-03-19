3cc512d2e8cc79491a30bf791e70e2c4b3adc51e
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-03-19 15:49:47 +0000

    Clean comments

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 /* 
 Rock paper scissors 
 
-Classic RPS vs the computer for 5 rounds
+Classic RPS vs the computer
 The winner of each round will be displayed and then the overall winner after
 the 5th round
-The overall winner is the one who wins 3 out of the 5 games
+The overall winner is the one who wins the most out of 5 rounds
 
 How to win:
 Player and component will choose either rock paper or scissors
@@ -57,55 +57,45 @@ function playRound(computerSelection, playerSelection) {
     console.log(computerSelection)
 
     // When Computer chooses Rock
-        // If you choose rock, it's a draw
     if ((computerSelection === "rock") && (playerSelection === "rock")) {
         console.log("Draw! Both Players chose Rock");
         return "draw";
-        // If you choose paper, you win
     } else if ((computerSelection === "rock") && (playerSelection === "paper")) {
         console.log("You Win! Paper beats Rock");
         return "win";
-        // If you choose scissors, you lose
     } else if ((computerSelection === "rock") && (playerSelection === "scissors")) {
         console.log("You Lose! Rock beats Scissors");
         return "lose";
     }
 
     // When Computer Chooses Paper
-        // If you choose rock, you lose
     if ((computerSelection === "paper") && (playerSelection === "rock")) {
         console.log("You Lose! Paper beats Rock");
         return "lose";
-        // If you choose paper, it's a draw
     } else if ((computerSelection === "paper") && (playerSelection === "paper")) {
         console.log("Draw! Both Players chose Paper");
         return "draw";
-        // If you choose scissors, you win
     } else if ((computerSelection === "paper") && (playerSelection === "scissors")) {
         console.log("You Win! Scissors beats Paper");
         return "win";
     }
 
     // When Computer Chooses Scissors
-        // If you choose rock, you win
     if ((computerSelection === "scissors") && (playerSelection === "rock")) {
         console.log("You Win! Rock beats Scissors");
         return "win";
-        // If you choose paper, you lose
     } else if ((computerSelection === "scissors") && (playerSelection === "paper")) {
         console.log("You Lose! Scissors beats Paper");
         return "lose";
-        // If you choose scissors, it's a draw
     } else if ((computerSelection === "scissors") && (playerSelection === "scissors")) {
         console.log("Draw! Both Players chose Scissors");
         return "draw";
     }
 }
     // Play a game of Rock, Paper Scissors
-    
+
     function playGame() {
         alert("Welcome to Rock Paper Scissors");
-        // Set two variables to record your score and the computer's score
         let playerScore = 0;
         let computerScore = 0;
         // Run the game 5 times
